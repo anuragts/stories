d5ff8c32902351fc4402d34c97267a64de01bb4e
Anurag30112003 <anuragsharma011011@gmail.com>
2022-11-02 16:54:52 +0530

    typo fix in footer

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <>
         <div className="text-center my-5">
-        Create by <a className="text-blue-400" href="https://twitter.com/theanuragdev">@theanuragdev</a>
+        Created by <a className="text-blue-400" href="https://twitter.com/theanuragdev">@theanuragdev</a>
         </div>
         </>
     )
