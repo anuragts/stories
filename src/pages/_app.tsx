43dd2381e9d6fab31852f1be147c6aa5f91c4fe1
Anurag30112003 <anuragsharma011011@gmail.com>
2022-11-02 17:09:43 +0530

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Layout from "./components/Layout";
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <Layout>
-  <Component {...pageProps} />
-  <Analytics/>
-  </Layout>
-  </>
-  )
+    <>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
+  );
 }
